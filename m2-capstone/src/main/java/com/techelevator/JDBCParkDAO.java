1839bb46fababc8300309af072f4814487a8edb7
kylehiers <hiers.3@buckeyemail.osu.edu>
2018-06-28 10:34:16 -0400

    updated park somewhat

---
+++ m2-capstone/src/main/java/com/techelevator/JDBCParkDAO.java
@@ -0,0 +1,39 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+public class JDBCParkDAO implements ParkDAO{
+	
+	private JdbcTemplate jdbctemplate;
+
+	@Override
+	public List<Park> getAllParks() {
+		
+		List<Park> allParks = new ArrayList<Park>();
+		String queryForAll = "select * from park";
+		SqlRowSet results = jdbctemplate.queryForRowSet(queryForAll);
+		
+		while(results.next()) {
+			allParks.add(mapParkToRow(results));
+		}
+		return allParks;
+	}
+	
+	
+	private Park mapParkToRow(SqlRowSet results) {
+		Park park = new Park();
+		park.setParkId(results.getInt("park_id"));
+		park.setParkName(results.getString("name"));
+		park.setDescription(results.getString("description"));
+		park.setEstDate(results.getString("establish_date"));
+		park.setParkArea(results.getInt("area"));
+		park.setVisitors(results.getInt("visitors"));
+		park.setParkLocation(results.getString("location"));
+		return park;
+	}
+
+}
---
+++ m2-capstone/src/main/java/com/techelevator/Park.java
@@ -0,0 +1,77 @@
+package com.techelevator;
+
+public class Park {
+	
+	private int parkId;
+	private String parkName;
+	private String parkLocation;
+	private String estDate;
+	private int visitors;
+	private String description;
+	private int parkArea;
+	
+	
+	public int getParkId() {
+		return parkId;
+	}
+	public void setParkId(int parkId) {
+		this.parkId = parkId;
+	}
+	
+	
+	
+	public String getParkName() {
+		return parkName;
+	}
+	public void setParkName(String parkName) {
+		this.parkName = parkName;
+	}
+	
+	
+	
+	public String getParkLocation() {
+		return parkLocation;
+	}
+	public void setParkLocation(String parkLocation) {
+		this.parkLocation = parkLocation;
+	}
+	
+	
+	
+	public String getEstDate() {
+		return estDate;
+	}
+	public void setEstDate(String estDate) {
+		this.estDate = estDate;
+	}
+	
+	
+	
+	public int getVisitors() {
+		return visitors;
+	}
+	public void setVisitors(int visitors) {
+		this.visitors = visitors;
+	}
+	
+	
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	public int getParkArea() {
+		return parkArea;
+	}
+	public void setParkArea(int parkArea) {
+		this.parkArea = parkArea;
+	}
+
+	
+	
+}
